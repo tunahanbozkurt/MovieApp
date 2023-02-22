a99be4513b90b7851536ce1a718814e35abaf04b
tunahanbozkurt <t.bozkurt5775@gmail.com>
2023-02-22 06:40:16 +0300

    firebase crashlytics and auth impl

--- app/src/main/java/com/example/movieapp/MainActivity.kt
+++ app/src/main/java/com/example/movieapp/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             MovieAppTheme {
                 OnboardingThirdScreen()
+                throw RuntimeException("Crashlytics test")
             }
         }
     }
